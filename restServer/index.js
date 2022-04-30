d09952369dd70ffaff0de6fab7a4aad500fa2a39
Corey <cenew16@gmail.com>
2022-04-29 21:31:19 -0700

    Server with static file

--- restServer/index.js
+++ restServer/index.js
@@ -1,6 +1,11 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+app.set('port', 3000);
 
-const cors = require('cors')
+app.use(express.static(__dirname));
 
-const mongo = require('mongodb')
-const MongoClient = require('mongodb').MongoClient
+app.listen(app.get('port'), function() {
+
+	console.log('Express server started on http://localhost:' + app.get('port'));
+	console.log(__dirname);
+})
